7a3017b98c428a5cb4c3455dd0909d4ee2c78260
Hui Jiang <hui1021@yahoo.com>
2017-06-20 05:04:20 +0000

    migration files updated

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,12 @@
 class Cart < ActiveRecord::Base
+  has_many :line_items
+  has_many :items, through: :line_items
+  belongs_to :user
 
+  def total
+    line_items.inject{ |sum, item| sum + item.total}
+  end
+
+  def add_item
+  end
 end
